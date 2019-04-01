1cd58238d904fcda1e8f4cb847cebef78488164e
306714577 <44558375+306714577@users.noreply.github.com>
2019-04-01 15:30:05 +0800

    Create 0121.Best Time to Buy and Sell Stock 买卖股票的最佳时机.cpp

---
+++ src/0121.Best Time to Buy and Sell Stock 买卖股票的最佳时机.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    const int inf = 0x3f3f3f3f;
+    int maxProfit(vector<int>& prices) {
+        int minn = inf;
+        int sum = 0;
+        for(auto p:prices){
+            minn = min(minn,p); //找出前i-1天最小的股票价格
+            sum = max(sum,p-minn);  //找出最大利润
+        }
+        return sum;
+    }
+};
+//确定状态
+//最后一步：通过一次交易，前第i天能获得的最大利益
+//子问题：通过一次交易，前第i-1天能获得的最大利益
+//状态转移方程
+//f[x] = max(f[x-1],prices[i]-minn)
+//前i天的最大收益 = max{前i-1天的最大收益，第i天的价格-前i-1天中的最小价格}
