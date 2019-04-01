75fc59985922eae8666729f2f557255d2af001e1
306714577 <44558375+306714577@users.noreply.github.com>
2019-04-01 20:01:27 +0800

    Update 0122.Best Time to Buy and Sell Stock II买卖股票的最佳时机2.cpp

--- src/0122.Best Time to Buy and Sell Stock II买卖股票的最佳时机2.cpp
+++ src/0122.Best Time to Buy and Sell Stock II买卖股票的最佳时机2.cpp
@@ -2,7 +2,7 @@ class Solution {    //贪心思想
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();  //天数n
-        int sum = 0;  
+        int sum = 0;
         for (int i = 0; i < n - 1; i++) //遍历所有天数
             if (prices[i] < prices[i + 1])  //对于每两天，若出现可赚取利润
                 sum += prices[i+1] - prices[i];   //则累加
@@ -14,13 +14,14 @@ class Solution {    //动态规划
 public:
     const int inf = 0x3f3f3f3f; //无穷大
     int maxProfit(vector<int>& prices) {
-        int profit = -inf;  //当前持有资金的正负值，取无穷小，找最大值
-        int sum = 0;        //最大利润
-        //动态规划思想
-        for(auto p:prices){ //遍历每一个股票价格并视作 “交易点”
-            profit = max(profit,sum - p);   //对当前点，有不做交易和做交易两种结果
-            sum = max(sum,profit+p);    //不做交易和做交易后的最大利润
+        int buy = -inf;  //当前持有资金的正负值，取无穷小，找最大值
+        int sell = 0;        //最大利润
+        for(auto p:prices){
+            //遍历每一天，并将每一个股票价格视作 “交易点”
+            //对"每一次交易"，更新 买入和卖出 的状态变量
+            buy  = max(buy,sell - p);   //更新买入股票的最大利益
+            sell = max(sell,buy + p);   //更新卖出股票的最大利益
         }
-        return sum;
+        return sell;
     }
 };
