1931206cad8efeecf89ce61a767f069df2e7d8c0
306714577 <44558375+306714577@users.noreply.github.com>
2019-04-01 19:49:16 +0800

    Update 0188.Best Time to Buy and Sell Stock IV买卖股票的最佳时机4.cpp

--- src/0188.Best Time to Buy and Sell Stock IV买卖股票的最佳时机4.cpp
+++ src/0188.Best Time to Buy and Sell Stock IV买卖股票的最佳时机4.cpp
@@ -13,14 +13,14 @@ private:
 public:
     const int inf = 0x3f3f3f3f;
     int maxProfit(int k,vector<int>& prices) {
-        /* *
+        /*
          * 最少两天完成一次交易，那么当k >= 天数/2时，相当于不限制次数
          * 这时候用 “II” 的贪心解法效率更高
          * 当k在区间(1,n/2)时，采用 “III” 的动态规划思想来解决,
          * 即对每一次交易，定义其 买入 和 卖出 时最大收益的状态变量，
          * 遍历整个所有价格，更新状态变量，t[i][0]和t[i][1]
          * 得到 t[k-1][1] 即为所求完成k次交易后的最大利益
-         * /
+         */
         if(k < 1) return 0;
         if(k >= prices.size()/2) return greedy(prices);
         int t[k][2];    // [2] 表示买入和卖出股票时最大利益的状态
