2eab6fcdf3b9bc9900c708255a93f26d5513ab63
RJman <44558375+306714577@users.noreply.github.com>
2019-04-02 08:43:27 +0800

    Create 0063.Unique Paths II 不同路径数2 with 障碍物.cpp

---
+++ src/0063.Unique Paths II 不同路径数2 with 障碍物.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();    //行数
+        int n = obstacleGrid[0].size(); //列数
+        if(m==0 || n == 0) return 0;
+        long long f[m][n];  //测试数据刁钻，开long long
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(obstacleGrid[i][j] == 1) f[i][j] = 0;
+                else{   //如果0行、0列上有为1的单元格，那么其后都为0
+                    if(i == 0 && j == 0) f[i][j] = 1;   //初始状态
+                    else{
+                        f[i][j] = 0;    //初始化为0
+                        if(i >= 1) f[i][j] += f[i-1][j];    //如果左边有路，那么+=
+                        if(j >= 1) f[i][j] += f[i][j-1];    //如果上边有路，那么+=
+                    }
+                }
+            }
+        }
+        return f[m-1][n-1];
+    }
+};
