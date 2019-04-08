8854f10a48de9434f6fb0ba868a09723eba49ae4
RJman <44558375+306714577@users.noreply.github.com>
2019-04-08 18:59:22 +0800

    Update and rename Maximum Depth of Binary Tree 二叉树的最大深度.cpp to 0104.Maximum Depth of Binary Tree 二叉树的最大深度.cpp

--- src/0104.Maximum Depth of Binary Tree 二叉树的最大深度.cpp
+++ src/0104.Maximum Depth of Binary Tree 二叉树的最大深度.cpp
@@ -10,6 +10,6 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        return root == NULL?0:max(maxDepth(root.left),maxDepth(root.right))+1;
+        return root == NULL?0:max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
