e3a40c5e657e188a56fa2cf19f3321dcb3f10bc7
RJman <44558375+306714577@users.noreply.github.com>
2019-04-14 20:28:14 +0800

    Update 0063.Unique Paths II 不同路径数2 with 障碍物.cpp

--- src/0063.Unique Paths II 不同路径数2 with 障碍物.cpp
+++ src/0063.Unique Paths II 不同路径数2 with 障碍物.cpp
@@ -8,9 +8,9 @@ public:
         long long f[m][n];  //测试数据刁钻，开long long
         for(int i = 0;i < m;i++){
             for(int j = 0;j < n;j++){
-                if(obstacleGrid[i][j] == 1) f[i][j] = 0;
-                else{   //如果0行、0列上有为1的单元格，那么其后都为0
-                    if(i == 0 && j == 0) f[i][j] = 1;   //初始状态
+                if(obstacleGrid[i][j] == 1) f[i][j] = 0;    //如果0行、0列上有为1的单元格，那么其后都为0
+                else{   
+                    if(i == 0 && j == 0) f[i][j] = 1;   //初始状态,第一行、第一列的情况
                     else{
                         f[i][j] = 0;    //初始化为0
                         if(i >= 1) f[i][j] += f[i-1][j];    //如果左边有路，那么+=
