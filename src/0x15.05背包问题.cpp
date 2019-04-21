e665a83c76a83f3554a344552540c35f0e46fdde
RJman <44558375+306714577@users.noreply.github.com>
2019-04-21 16:00:59 +0800

    Create 0x15.05背包问题.cpp

---
+++ src/0x15.05背包问题.cpp
@@ -0,0 +1,46 @@
+//由于采用一维度数组，未保存先前状态
+//未完成本题的计算可获取物品的最大数量
+//找时间补充
+
+// 01背包+方案数+一维优化
+//状态转移方程:f[i][j] = f[i-1][j] + f[i-1][j-bp[i-1]]
+//滚动数组优化:f[now][j] = f[old][j] + f[old][j-bp[old]]
+//一维数组优化:f[j] = f[j] + f[j-bp[i-1]]
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m;
+
+int buyTheGoods(vector<int> &bp,int m){
+    int n = bp.size();
+    if(!n || !m) return 0;
+    int f[m+1];
+    f[0] = 1;
+    for(int i = 1;i <= m;i++) f[i] = 0;
+    for(int i = 1;i <= n;i++){
+        for(int j = m;j >= 0;j--){
+            if(j >= bp[i-1]){
+                f[j] += f[j - bp[i-1]];
+            }
+        }
+    }
+    return f[m];
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        cin >> n >> m;
+        vector<int> bp;
+        for(int i = 0;i < n;i++){
+            int temp;
+            cin >> temp;
+            bp.push_back(temp);
+        }
+        int ret = buyTheGoods(bp);
+        cout << ret << endl;
+    }
+    return 0;
+}
