5dfa5ea22536f1fb84c9d01815197bb6ee9641ac
RJman <44558375+306714577@users.noreply.github.com>
2019-04-29 21:07:08 +0800

    Create 0x22.找球号(二).cpp

---
+++ src/DataStructure/0x22.找球号(二).cpp
@@ -0,0 +1,54 @@
+//找球号(二)----Hash_Table
+#include<stdio.h>
+#include<string.h>
+const int N =1000002;
+const int fib = 111123;
+static int hash[N],head[N],next[N];
+static int top;
+//添加链表元素
+void add(int m){
+    int key = m % fib;      //球 m 对应的特征值
+    next[top] = head[key];    //next[0] =  head[key] = -1;
+    head[key] = top;  //head[key] = 0;
+    hash[top] = m;    //hash[0] = 球m;
+    top++;  //top = 1;
+}
+//查询元素
+bool query(int m){
+    int key = m % fib;
+    for(int i = head[key]; i > -1;i = next[i]){ //遍历具有同一key值的next表元素
+        if(hash[i]==m) return true;
+    }
+    return false;
+}
+int main(){
+    int n,m;
+    int num;
+    char s[6];
+    scanf("%d",&n);
+    memset(head,-1,sizeof(head));   //重置链表头指针为-1
+    top = 0;//写在外面，只建立一个hash表
+    while(n--){
+        scanf("%s%d",s,&m);
+        if(s[0]=='A'){
+            for(int i=0;i<m;i++){
+                scanf("%d",&num);
+                add(num);
+            }
+        }
+        else{
+            for(int i=0;i<m;i++){
+                scanf("%d",&num);
+                if(query(num))
+                printf("YES\n");
+                else printf("NO\n");
+            }
+        }
+    }
+    return 0;
+}
+//head数组是每个哈希值(hashcode/特征值)的链表头指针，next数组就是链表的next指针
+//每个hashcode(key)存在一个链表,用next数组作索引指向具有相同key的元素
+//head作为头指针,对每一个hashcode,存储它的头元素(此处采用最后进入链表的元素)
+//hash表存储元素的原始数值,通过判断hash[head[key]] == m?即是否有相应哈希值的链表元素与m匹配
+//得到哈希表中是否存在某个元素
